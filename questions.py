3022f5d3a45a23cd3e579f4b6a38fe8941734bef
lexa.shvetsoff <lexa.shvetsoff@yandex.ru>
2023-03-19 23:30:18 +0800

    Более надежный код

--- questions.py
+++ questions.py
@@ -1,11 +1,11 @@
 import re
 
-def get_questions(file_path, questions):
+def get_questions(file_path):
     with open(file_path, 'r', encoding='KOI8-R') as file:
         file_content = file.read()
 
     split_file_content = re.split('\n{3}', file_content)
-    
+    temp_questions = {}
     for item in split_file_content:
         split_items = re.split('\n{2}', item)
         key_question = ''
@@ -16,13 +16,14 @@ def get_questions(file_path, questions):
             if 'Ответ' in split_item:
                 value_question = re.split(':\n', split_item)[1]
             if key_question and value_question:
-                questions[key_question] = value_question
+                temp_questions[key_question] = value_question
+    return temp_questions
 
 
 def union_questions(files_path):
     questions = {}
 
     for file_path in files_path:
-        get_questions(file_path, questions)
+        questions.update(get_questions(file_path))
     
     return questions
